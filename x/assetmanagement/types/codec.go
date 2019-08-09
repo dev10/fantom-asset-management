353d48a03e988a122ef0cb7a843d342e696e306f
Peter Badenhorst <dev10@mispah.net>
2019-08-09 14:49:45 +0200

    Register codec for new messages

---
+++ x/assetmanagement/types/codec.go
@@ -0,0 +1,17 @@
+package types
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+var ModuleCdc = codec.New()
+
+func init() {
+	RegisterCodec(ModuleCdc)
+}
+
+// RegisterCodec registers concrete types on the Amino codec
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterConcrete(MsgIssueToken{}, "assetmanagement/IssueToken", nil)
+	cdc.RegisterConcrete(MsgMintCoins{}, "assetmanagement/MintCoins", nil)
+}
