4aa1f062fddf3744939d0fa57860212bbe802302
Peter Badenhorst <dev10@mispah.net>
2019-08-12 14:01:11 +0200

    use consistent name for owner

--- x/assetmanagement/types/messages.go
+++ x/assetmanagement/types/messages.go
@@ -59,15 +59,15 @@ func (msg MsgIssueToken) GetSigners() []sdk.AccAddress {
 type MsgMintCoins struct {
 	Amount string         `json:"amount"`
 	Symbol string         `json:"symbol"`
-	Minter sdk.AccAddress `json:"minter"`
+	Owner  sdk.AccAddress `json:"owner"`
 }
 
 // NewMsgMintCoins is the constructor function for MsgMintCoins
-func NewMsgMintCoins(amount, symbol string, minter sdk.AccAddress) MsgMintCoins {
+func NewMsgMintCoins(amount, symbol string, owner sdk.AccAddress) MsgMintCoins {
 	return MsgMintCoins{
 		Amount: amount,
 		Symbol: symbol,
-		Minter: minter,
+		Owner:  owner,
 	}
 }
 
@@ -79,8 +79,8 @@ func (msg MsgMintCoins) Type() string { return "mint_coins" }
 
 // ValidateBasic runs stateless checks on the message
 func (msg MsgMintCoins) ValidateBasic() sdk.Error {
-	if msg.Minter.Empty() {
-		return sdk.ErrInvalidAddress(msg.Minter.String())
+	if msg.Owner.Empty() {
+		return sdk.ErrInvalidAddress(msg.Owner.String())
 	}
 	if len(msg.Symbol) == 0 || len(msg.Amount) == 0 {
 		return sdk.ErrUnknownRequest("Symbol and/or Amount cannot be empty")
@@ -98,22 +98,22 @@ func (msg MsgMintCoins) GetSignBytes() []byte {
 
 // GetSigners defines whose signature is required
 func (msg MsgMintCoins) GetSigners() []sdk.AccAddress {
-	return []sdk.AccAddress{msg.Minter}
+	return []sdk.AccAddress{msg.Owner}
 }
 
 // MsgBurnCoins defines the BurnCoins message
 type MsgBurnCoins struct {
 	Amount string         `json:"amount"`
 	Symbol string         `json:"symbol"`
-	Source sdk.AccAddress `json:"source"`
+	Owner  sdk.AccAddress `json:"owner"`
 }
 
 // NewMsgBurnCoins is the constructor function for MsgBurnCoins
-func NewMsgBurnCoins(amount, symbol string, source sdk.AccAddress) MsgBurnCoins {
+func NewMsgBurnCoins(amount, symbol string, owner sdk.AccAddress) MsgBurnCoins {
 	return MsgBurnCoins{
 		Amount: amount,
 		Symbol: symbol,
-		Source: source,
+		Owner:  owner,
 	}
 }
 
@@ -125,8 +125,8 @@ func (msg MsgBurnCoins) Type() string { return "burn_coins" }
 
 // ValidateBasic runs stateless checks on the message
 func (msg MsgBurnCoins) ValidateBasic() sdk.Error {
-	if msg.Source.Empty() {
-		return sdk.ErrInvalidAddress(msg.Source.String())
+	if msg.Owner.Empty() {
+		return sdk.ErrInvalidAddress(msg.Owner.String())
 	}
 	if len(msg.Symbol) == 0 || len(msg.Amount) == 0 {
 		return sdk.ErrUnknownRequest("Symbol and/or Amount cannot be empty")
@@ -144,22 +144,22 @@ func (msg MsgBurnCoins) GetSignBytes() []byte {
 
 // GetSigners defines whose signature is required
 func (msg MsgBurnCoins) GetSigners() []sdk.AccAddress {
-	return []sdk.AccAddress{msg.Source}
+	return []sdk.AccAddress{msg.Owner}
 }
 
 // MsgFreezeCoins defines the FreezeCoins message
 type MsgFreezeCoins struct {
 	Amount string         `json:"amount"`
 	Symbol string         `json:"symbol"`
-	Source sdk.AccAddress `json:"source"`
+	Owner  sdk.AccAddress `json:"owner"`
 }
 
 // NewMsgFreezeCoins is the constructor function for MsgFreezeCoins
-func NewMsgFreezeCoins(amount, symbol string, source sdk.AccAddress) MsgFreezeCoins {
+func NewMsgFreezeCoins(amount, symbol string, owner sdk.AccAddress) MsgFreezeCoins {
 	return MsgFreezeCoins{
 		Amount: amount,
 		Symbol: symbol,
-		Source: source,
+		Owner:  owner,
 	}
 }
 
@@ -171,8 +171,8 @@ func (msg MsgFreezeCoins) Type() string { return "freeze_coins" }
 
 // ValidateBasic runs stateless checks on the message
 func (msg MsgFreezeCoins) ValidateBasic() sdk.Error {
-	if msg.Source.Empty() {
-		return sdk.ErrInvalidAddress(msg.Source.String())
+	if msg.Owner.Empty() {
+		return sdk.ErrInvalidAddress(msg.Owner.String())
 	}
 	if len(msg.Symbol) == 0 || len(msg.Amount) == 0 {
 		return sdk.ErrUnknownRequest("Symbol and/or Amount cannot be empty")
@@ -190,22 +190,22 @@ func (msg MsgFreezeCoins) GetSignBytes() []byte {
 
 // GetSigners defines whose signature is required
 func (msg MsgFreezeCoins) GetSigners() []sdk.AccAddress {
-	return []sdk.AccAddress{msg.Source}
+	return []sdk.AccAddress{msg.Owner}
 }
 
 // MsgUnfreezeCoins defines the UnfreezeCoins message
 type MsgUnfreezeCoins struct {
 	Amount string         `json:"amount"`
 	Symbol string         `json:"symbol"`
-	Source sdk.AccAddress `json:"source"`
+	Owner  sdk.AccAddress `json:"owner"`
 }
 
 // NewMsgUnfreezeCoins is the constructor function for MsgUnfreezeCoins
-func NewMsgUnfreezeCoins(amount, symbol string, source sdk.AccAddress) MsgUnfreezeCoins {
+func NewMsgUnfreezeCoins(amount, symbol string, owner sdk.AccAddress) MsgUnfreezeCoins {
 	return MsgUnfreezeCoins{
 		Amount: amount,
 		Symbol: symbol,
-		Source: source,
+		Owner:  owner,
 	}
 }
 
@@ -217,8 +217,8 @@ func (msg MsgUnfreezeCoins) Type() string { return "unfreeze_coins" }
 
 // ValidateBasic runs stateless checks on the message
 func (msg MsgUnfreezeCoins) ValidateBasic() sdk.Error {
-	if msg.Source.Empty() {
-		return sdk.ErrInvalidAddress(msg.Source.String())
+	if msg.Owner.Empty() {
+		return sdk.ErrInvalidAddress(msg.Owner.String())
 	}
 	if len(msg.Symbol) == 0 || len(msg.Amount) == 0 {
 		return sdk.ErrUnknownRequest("Symbol and/or Amount cannot be empty")
@@ -236,5 +236,5 @@ func (msg MsgUnfreezeCoins) GetSignBytes() []byte {
 
 // GetSigners defines whose signature is required
 func (msg MsgUnfreezeCoins) GetSigners() []sdk.AccAddress {
-	return []sdk.AccAddress{msg.Source}
+	return []sdk.AccAddress{msg.Owner}
 }
