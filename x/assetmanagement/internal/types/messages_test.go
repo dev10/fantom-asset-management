19316402a153bd36e12e81eb5f6d4fad73731769
Peter Badenhorst <dev10@mispah.net>
2019-09-25 17:22:24 +0200

    add token issue validation tests

--- x/assetmanagement/internal/types/messages_test.go
+++ x/assetmanagement/internal/types/messages_test.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"fmt"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -18,4 +19,41 @@ func TestMsgIssueToken(t *testing.T) {
 
 	require.Equal(t, msg.Route(), RouterKey)
 	require.Equal(t, msg.Type(), "issue_token")
+}
+
+func TestMsgIssueTokenValidation(t *testing.T) {
+	var (
+		name               = "Zap"
+		symbol             = "ZAP"
+		total        int64 = 1
+		totalInvalid int64 = 0
+		acc                = sdk.AccAddress([]byte("me"))
+		name2              = "a"
+		total2       int64 = 2
+		acc2               = sdk.AccAddress([]byte("you"))
+	)
+
+	cases := []struct {
+		valid bool
+		tx    MsgIssueToken
+	}{
+		{true, NewMsgIssueToken(acc, name, symbol, total, false)},
+		{true, NewMsgIssueToken(acc, name, symbol, total, false)},
+		{false, NewMsgIssueToken(acc, name, symbol, totalInvalid, false)},
+		{true, NewMsgIssueToken(acc2, name2, symbol, total2, false)},
+		{true, NewMsgIssueToken(acc2, name2, symbol, total, false)},
+		{true, NewMsgIssueToken(acc, name2, symbol, total2, false)},
+		{false, NewMsgIssueToken(nil, name, symbol, total2, false)},
+		{false, NewMsgIssueToken(acc2, "", symbol, total2, false)},
+		{false, NewMsgIssueToken(acc2, name, symbol, totalInvalid, false)},
+	}
+
+	for _, tc := range cases {
+		err := tc.tx.ValidateBasic()
+		if tc.valid {
+			require.Nil(t, err, fmt.Sprintf("transaction [%v] failed but was marked valid", tc))
+		} else {
+			require.NotNil(t, err, fmt.Sprintf("transaction [%v] is valid but has an error", tc))
+		}
+	}
 }
